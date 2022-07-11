4cfa9e6b61342b74854204f37fe4484bae3beaaf
qianzhikang <qzkqianzhikang@gmail.com>
2022-07-11 13:20:57 +0800

    ref获取html标签元素

---
+++ code/Vue3/vue3_study/src/08_ref获取html标签元素/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>ref获取页面中的元素</h2>
+    <input type="text" ref="inputRef"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent,onMounted,ref } from "vue";
+export default defineComponent({
+  name: "App",
+  // 需求：当页面加载后，页面中的文本框可以自动获取焦点
+  setup() {
+    const inputRef = ref<HTMLElement | null>(null)
+    // 页面加载后的生命周期
+    onMounted(()=>{
+      inputRef.value&&inputRef.value.focus() //自动获取焦点
+    })
+    return {
+      inputRef
+    };
+  },
+});
+</script>
+
+<style></style>
